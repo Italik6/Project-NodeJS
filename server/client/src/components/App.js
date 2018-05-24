e7011cf0a3ac938745e0ef0bb74c1b347a2532e1
Michal Turek <michael.turek@rizk.com>
2018-05-24 13:50:47 +0200

    first-action

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+import Header from './Header';
 
 const App = () => {
   return (
-    <div>
+    <div className="container">
       <BrowserRouter>
         <div>
           <Header />
-          <Route exact path="/" component={Landing} />
+          {/* <Route exact path="/" component={Landing} />
           <Route exact path="/surveys" component={Dashboard} />
-          <Route path="/surveys/new" component={SurveyNew} />
+          <Route path="/surveys/new" component={SurveyNew} /> */}
         </div>
       </BrowserRouter>
     </div>
