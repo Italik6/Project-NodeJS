b0d679732c2b8a9628b133ce5543932d672881d7
Michal Turek <michael.turek@rizk.com>
2018-05-24 11:35:57 +0200

    routes

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 
 const App = () => {
-  return <div>Hi there!</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route path="/surveys/new" component={SurveyNew} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
