8b42c611ed9689b289e71c4ea41879c6f5805729
Michal Turek <michael.turek@rizk.com>
2018-05-24 14:37:57 +0200

    refactor-of-async-function

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,20 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-import Header from './Header';
+import Header from "./Header";
+import { connect } from "react-redux";
+import * as actions from "../actions";
 
-const App = () => {
-  return (
-    <div className="container">
-      <BrowserRouter>
-        <div>
-          <Header />
-          {/* <Route exact path="/" component={Landing} />
-          <Route exact path="/surveys" component={Dashboard} />
-          <Route path="/surveys/new" component={SurveyNew} /> */}
-        </div>
-      </BrowserRouter>
-    </div>
-  );
-};
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            {/* <Route exact path="/" component={Landing} />
+                  <Route exact path="/surveys" component={Dashboard} />
+                  <Route path="/surveys/new" component={SurveyNew} /> */}
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
 
-export default App;
+export default connect(null, actions)(App);
