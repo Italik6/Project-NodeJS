d422d46ddc971368633a17fc5c2eb6a4ada9a8f6
Michal Turek <michael.turek@rizk.com>
2018-06-01 11:53:45 +0200

    advancing-surveyform

--- server/client/src/components/surveys/SurveyForm.js
+++ server/client/src/components/surveys/SurveyForm.js
@@ -30,7 +30,9 @@ class SurveyForm extends Component {
   render() {
     return (
       <div>
-        <form onSubmit={this.propshandleSubmit(values => console.log(values))}>
+        <form
+          onSubmit={this.props.handleSubmit(() => this.props.onSurveySubmit())}
+        >
           {this.renderFields()}
           <Link to="/surveys" className="red btn-flat white-text">
             Cancel
