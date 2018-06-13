9e0abe4d3a4013aeb3be8e117ab08cc6e39335ef
Michal Turek <michael.turek@rizk.com>
2018-06-13 16:18:15 +0200

    refactor-with-chain

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -15,10 +15,22 @@ module.exports = app => {
   });
 
   app.post("/api/surveys/webhooks", (req, res) => {
-    const events = _.map(req.body, event => {
-      const pathname = new URL(event.url).pathname;
-      const p = new Path("/api/surveys/:surveyId/:choice");
-    });
+    const p = new Path("/api/surveys/:surveyId/:choice");
+    const events = _
+      .chain(req.body)
+      .map(({ email, url }) => {
+        const match = p.test(new URL(url).pathname);
+        if (match) {
+          return {
+            email,
+            surveyId: match.surveyId,
+            choice: match.choice
+          };
+        }
+      })
+      .compact()
+      .uniqBy("email", "surveyId")
+      .value();
   });
 
   app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
