e58eb61cc0fbc6988ba0ee54b3b60bfc0971f2d7
Michal Turek <michael.turek@rizk.com>
2018-06-20 14:55:29 +0200

    change-main-page

--- server/client/src/components/Landing.js
+++ server/client/src/components/Landing.js
@@ -3,8 +3,8 @@ import React from "react";
 const Landing = () => {
   return (
     <div style={{ textAlign: "center" }}>
-      <h1>Emaily!</h1>
-      Collect feedback form your users
+      <h1>Emaily App!</h1>
+      Collect feedback from your users. It's easy!
     </div>
   );
 };
